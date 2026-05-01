dffba89035f91311f3786c3e7e0271a1cd6b906f
JuanPoNZEtti <juanponzetti5@gmail.com>
2026-05-01 01:45:44 -0300

    Actualizacion 2

---
+++ Ejercicios de JavaScript/Ejercicio1.js
@@ -0,0 +1,3 @@
+const integrantes = "Aaron, Diego, Juan y Camila";
+
+console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript`);
---
+++ Ejercicios de JavaScript/Ejercicio5.js
@@ -0,0 +1,10 @@
+let hora= 3;
+let minuto= 37;
+
+const segundosTotales= (hora*3600)+(minuto*60);
+
+//Horas y minutos
+console.log(`Son ${hora} horas y ${minuto} minutos`);
+
+//Segundos
+console.log(`Y con ${segundosTotales} segundos`);
