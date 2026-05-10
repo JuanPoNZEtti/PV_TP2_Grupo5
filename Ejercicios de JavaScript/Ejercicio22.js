2067011139cf13069fb8bdb9e1e83028c10a88f8
JuanPoNZEtti <juanponzetti5@gmail.com>
2026-05-10 12:02:21 -0300

    Nueva Actulizacion 3 Ponzetti Juan

---
+++ Ejercicios de JavaScript/Ejercicio22.js
@@ -0,0 +1,13 @@
+const entrada = document.getElementById("miInput");
+
+const salida = document.getElementById("miParrafo");
+
+const TextoSalida = (entrada, salida) => {
+    const texto = entrada.value;
+    salida.textContent = texto;
+    console.log(`${texto}`);
+};
+
+entrada.addEventListener("input", () => {
+    TextoSalida(entrada, salida);
+});
