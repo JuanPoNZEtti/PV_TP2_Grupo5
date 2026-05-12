f7ba3ecd59665606ea0296eec2f8db30d20947f2
Aaron Meriles <aaronmeriles@gmail.com>
2026-05-11 22:46:03 -0300

    se exportó la funcionalidad TextoSalida para el ejercicio23

--- Ejercicios de JavaScript/Ejercicio22.js
+++ Ejercicios de JavaScript/Ejercicio22.js
@@ -2,12 +2,13 @@ const entrada = document.getElementById("miInput");
 
 const salida = document.getElementById("miParrafo");
 
-const TextoSalida = (entrada, salida) => {
+export const TextoSalida = (entrada, salida) => {
     const texto = entrada.value;
     salida.textContent = texto;
     console.log(`${texto}`);
 };
-
+if(entrada && salida) {
 entrada.addEventListener("input", () => {
     TextoSalida(entrada, salida);
 });
+}
